b9eefba43a2786dd4922c4b5530f41679db1c587
Scott Fryxell <scott@copper.is>
2013-01-14 19:47:34 -0800

    using the page mod stuff instead.

--- user-agents/firefox/chrome/button.js
+++ user-agents/firefox/chrome/button.js
@@ -1,13 +1,12 @@
+var pageMod = require("page-mod");
+pageMod.PageMod({
+  include: ['*.copper.is','copper.dev','copper-stage.herokuapp.com'],
+  contentScript: '$(document).trigger("copper_button_installed")'
+});
 CustomButton = {
   1: function () {
     var s = window.content.document.createElement('script');
     s.src = 'https://www.copper.is/embed_iframe.js';
     window.content.document.body.appendChild(s);
   },
-}
-
-if (window.content.location.host == 'copper.is' || 'copper.dev' || 'copper-stage.herokuapp.com/'){
-  var s = window.content.document.createElement('script');
-  s.innerHTML = '$(document).trigger("copper_button_installed")';
-  window.content.document.body.appendChild(s); 
 }
