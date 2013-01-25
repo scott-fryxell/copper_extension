0cbead04b9dfb2ed09e06be2cfb07641da24ad69
Scott Fryxell <scott@copper.is>
2013-01-24 20:52:11 -0800

    new version of copper for safari.
    let's the user know when it's installed and where to look for teh
    extension.

--- user-agents/safari.safariextension/tip_this_page.js
+++ user-agents/safari.safariextension/tip_this_page.js
@@ -1,5 +1,5 @@
 safari.self.addEventListener("message", function (msgEvent){
-  if (msgEvent.name === "tip_this_page"){    
+  if (msgEvent.name === "tip_this_page"){   
     if (window.top === window) {
       var s = document.createElement('script');
       s.src = 'https://www.copper.is/embed_iframe.js';
@@ -8,7 +8,8 @@ safari.self.addEventListener("message", function (msgEvent){
   }
 }, false);
 
-if (window.location.host == 'copper.is' || window.location.host ==  'copper.dev' || window.location.host ==  'copper-stage.herokuapp.com'){
+var host = window.location.host
+if (host == 'copper.is' || host ==  'copper.dev' || host ==  'copper-stage.herokuapp.com'){
   var s = window.document.createElement('script');
   s.innerHTML = '$(document).trigger("copper_button_installed")';
   window.document.body.appendChild(s); 
